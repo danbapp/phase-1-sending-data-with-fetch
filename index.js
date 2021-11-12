809500567f18f8c848c85c95b9b1601218be6657
Daniel <danbapp@gmail.com>
2021-11-12 11:39:37 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const toServer = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+        Accept: 'application/json',
+    },
+    body:  JSON.stringify({
+      name: 'Daniel',
+     email: 'danbapp@gmail.com',
+    })
+  }
+
+// fetch("http://localhost:3000/users", toServer)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Error!");
+//     console.log(error.message);
+//   });
